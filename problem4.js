02789d580de51ad2ea02dcfaa2b439b60f15f708
jagdish <jagdishmahanta71@gmail.com>
2022-10-11 07:27:22 +0530

    js variables

---
+++ problem4.js
@@ -0,0 +1,15 @@
+// Problem 4
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+let School = "KERALA ENGLISH MEDIUM SCHOOL";
+let Name = "Jagdish Mahanta";
+let RollNo = 17;
+let Section = "A";
+let Grade = "A";
+let Maths = 88;
+let Physics = 69;
+let Chemistry = 72;
+console.log( "School  : "  + School  );
+console.log(("Name : "+ Name),("RollNo : " + RollNo),("Section : " + Section),("Grade : " + Grade),("Marks Scored :  Maths : "+ Maths) , ("   Physics : "+ Physics),("   Chemistry  : "+ Chemistry));
